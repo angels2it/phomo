36719bd15a813865f3dc8f29638b0f60151e1e34
Cuong Pham <angels2it@gmail.com>
2017-11-03 21:19:40 +0700

    ssl

--- config/env/production.js
+++ config/env/production.js
@@ -5,11 +5,10 @@ var fs = require('fs');
 module.exports = {
   secure: {
     ssl: true,
-    privateKey: './config/sslcerts/key.pem',
-    certificate: './config/sslcerts/cert.pem',
-    caBundle: './config/sslcerts/cabundle.crt'
+    privateKey: './config/sslcerts/phomo.key',
+    certificate: './config/sslcerts/phomo.me.crt'
   },
-  port: process.env.PORT || 8443,
+  port: process.env.PORT || 443,
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
